ca1bfccd0fc5ff54ed4ea869d2ed48066f5386f9
cote2porc <robin.denni@epitech.eu>
2024-09-21 14:34:00 +0900

    test(Mobile_Android) test the linter if i change something in the maintActivity.kt

--- Code/Mobile_Android/hooks/pre-commit.sh
+++ Code/Mobile_Android/hooks/pre-commit.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+readonly MOBILE_ANDROID_PATH=Code/Mobile_Android/
+CURRENT_DIR=$(pwd)
+
+# Check if the current directory ends with "Code/Mobile_Android"
+if [[ "$CURRENT_DIR" == */Code/Mobile_Android ]]; then
+    echo "You are in the correct directory: $CURRENT_DIR"
+else
+    echo "You are not in Code/Mobile_Android. Current directory is: $CURRENT_DIR"
+	cd $MOBILE_ANDROID_PATH || (echo "The current directory is not the good for this script, exiting" && exit 1)
+fi
+
+
 echo "Running ktlint..."
 ./gradlew ktlintCheck || exit 1
 
@@ -7,4 +19,4 @@ echo "Running ktlint..."
 echo "Running project build..."
 ./gradlew build || exit 1
 
-echo "All checks passed!"
+echo "All checks passed!, ready to push !"
