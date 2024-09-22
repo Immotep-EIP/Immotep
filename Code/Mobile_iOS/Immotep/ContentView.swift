0f1c35f6a6362c64d2012e9ee7dbfbe2bb01a2dd
AlessioLieb <ALESSIO2003@OUTLOOK.FR>
2024-09-22 17:40:16 +0200

    feat(Mobile_iOS): create the iOS mobile app using Xcode (iOS 18, Xcode 16)

---
+++ Code/Mobile_iOS/Immotep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Immotep
+//
+//  Created by Liebenguth Alessio on 22/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
