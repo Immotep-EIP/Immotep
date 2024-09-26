3bd659debaec64dbea5c7c95757288d741cc92eb
Mazettt <martin.d-herouville@epitech.eu>
2024-09-26 17:50:21 +0200

    test(backend): update script to launch tests -> print summary of coverage at the end

--- Code/Server/run_tests.sh
+++ Code/Server/run_tests.sh
@@ -3,5 +3,10 @@
 # Run tests and generate coverage report for all packages except prisma
 go test `go list ./... | grep -v prisma` -coverprofile cover.out
 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 # Generate coverage htlm report
+go tool cover -func=cover.out
 go tool cover -html=cover.out $@
