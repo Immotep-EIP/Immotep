cdfd0bcefb7a74030a4c3829b7f506e6590bbb46
cote2porc <robin.denni@epitech.eu>
2024-09-29 09:34:06 +0900

    feat(LoginScreen&&strings) base for the internationalization, add a first view with textfields and a button

--- Code/Mobile_Android/app/src/main/java/com/example/immotep/login/LoginScreen.kt
+++ Code/Mobile_Android/app/src/main/java/com/example/immotep/login/LoginScreen.kt
@@ -3,6 +3,7 @@ package com.example.immotep.login
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -10,8 +11,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.immotep.R
 
 @Composable
 fun LoginScreen(
@@ -24,13 +29,24 @@ fun LoginScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text("Login Screen")
-        TextField(label = { Text("Entrez votre email") }, value = emailAndPassword.value.email, onValueChange = { value ->
-            viewModel.updateEmailAndPassword(value, null)
-        })
-        TextField(label = { Text("Entrez votre mot de passe") }, value = emailAndPassword.value.password, onValueChange = { value ->
-            viewModel.updateEmailAndPassword(null, value)
-        })
-        Button(onClick = { navController.navigate("dashboard") }) { (Text("Se connecter")) }
+        Text(stringResource(R.string.login_hello))
+        TextField(
+            label = { Text(stringResource(R.string.your_email)) },
+            value = emailAndPassword.value.email,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            onValueChange = { value ->
+                viewModel.updateEmailAndPassword(value, null)
+            }
+        )
+        TextField(
+            label = { Text(stringResource(R.string.your_password)) },
+            value = emailAndPassword.value.password,
+            onValueChange = { value ->
+                viewModel.updateEmailAndPassword(null, value)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(onClick = { navController.navigate("dashboard") }) { Text(stringResource(R.string.login_button)) }
     }
 }
