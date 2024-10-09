c0743dc59f76555bff83eb94fbe4184aeffef0c3
Mazettt <martin.d-herouville@epitech.eu>
2024-10-09 11:54:05 +0200

    build(backend): fix update.sh script to generate also Prisma Client code

--- Code/update.sh
+++ Code/update.sh
@@ -8,6 +8,7 @@ fi
 
 cd Server/
 go run github.com/steebchen/prisma-client-go migrate deploy
+go run github.com/steebchen/prisma-client-go generate
 if [ $? -ne 0 ]; then
     echo "Prisma migration failed"
     exit 1
