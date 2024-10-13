f2e9b37ac391764275258421e90048166cb20967
AlessioLieb <ALESSIO2003@OUTLOOK.FR>
2024-10-13 18:22:00 +0200

    feat(CustomTextInput component): Add new custom component to avoid code repetition + have clearer files

---
+++ Code/Mobile_iOS/Immotep/Sources/Components/CustomTextInput.swift
@@ -0,0 +1,35 @@
+//
+//  CustomTextInput.swift
+//  Immotep
+//
+//  Created by Liebenguth Alessio on 13/10/2024.
+//
+
+// Custom text input for now used in login and register views, see if works well with UI tests
+
+import SwiftUI
+
+struct CustomTextInput: View {
+    var title: String
+    var placeholder: String
+    @Binding var text: String
+    var isSecure: Bool = false
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(title)
+                .font(.system(size: 14))
+                .frame(alignment: .leading)
+
+            if isSecure {
+                SecureField(placeholder, text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.system(size: 14))
+            } else {
+                TextField(placeholder, text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.system(size: 14))
+            }
+        }
+    }
+}
