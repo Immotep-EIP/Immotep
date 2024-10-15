db4614e5f9fcf5e2c7c018a4a4949c65804d3de6
Mazettt <martin.d-herouville@epitech.eu>
2024-10-15 11:55:17 +0200

    feat(backend router): add router rate limiter to reduce charge and attacks

--- Code/Server/router/router.go
+++ Code/Server/router/router.go
@@ -12,20 +12,28 @@ import (
 	"github.com/maxzerbini/oauth"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/ulule/limiter/v3"
+	mgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
 func Routes() *gin.Engine {
 	secretKey := os.Getenv("SECRET_KEY")
-
-	r := gin.New()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-
 	bServer := oauth.NewOAuthBearerServer(
 		secretKey,
 		time.Hour*24,
 		&TestUserVerifier{},
 		nil)
+
+	rate := limiter.Rate{
+		Period: 1 * time.Hour,
+		Limit:  3000,
+	}
+
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(mgin.NewMiddleware(limiter.New(memory.NewStore(), rate)))
 
 	r.GET("/", func(c *gin.Context) { c.String(http.StatusOK, "Welcome to Immotep API") })
 	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
