82a094709f6e6ded2f89b6f2d0ccd1ae5200d7d4
Lucas Binder <lucas.binder@epitech.eu>
2024-10-18 17:32:15 -0400

    feat(api example) Add call API example in  overview page

--- Code/Web/src/views/Overview/Overview.tsx
+++ Code/Web/src/views/Overview/Overview.tsx
@@ -1,10 +1,40 @@
-import React from "react";
-import style from './Overview.module.css';
+import React, { useEffect, useState } from 'react'
 
-const Overview: React.FC = () => (
+import getUsers from '@/services/api/User/GetUsers'
+import style from './Overview.module.css'
+
+const Overview: React.FC = () => {
+  const [data, setData] = useState<any>(null)
+
+  // API EXAMPLE
+  useEffect(() => {
+    const getInfo = async () => {
+      try {
+        const users = await getUsers()
+        setData(users)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    getInfo()
+  }, [])
+
+  return (
     <div className={style.layoutContainer}>
-      Overview
+      {data && Array.isArray(data) ? (
+        data.map((user: any) => (
+          <div key={user.id}>
+            <p>First Name: {user.firstname}</p>
+            <p>Last Name: {user.lastname}</p>
+            <p>Email: {user.email}</p>
+            <hr />
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  );
+  )
+}
 
-export default Overview;
+export default Overview
