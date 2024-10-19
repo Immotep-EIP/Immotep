91bd40f6bb7d695bcfe669b23106b2dbac0233d0
Lucas Binder <lucas.binder@epitech.eu>
2024-10-19 11:59:41 -0400

    test(login and register tests) add login and register tests with 92% of line coverage

--- Code/Web/jest.config.ts
+++ Code/Web/jest.config.ts
@@ -1,10 +1,16 @@
 export default {
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: false
+      }
+    ]
   },
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
