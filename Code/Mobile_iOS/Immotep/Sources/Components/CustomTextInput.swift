eae683652bba476c421fbb2689c335bc9da922c2
AlessioLieb <ALESSIO2003@OUTLOOK.FR>
2024-10-27 00:48:01 +0200

    feat(TaskBar & TopBar comps): add TaskBar and TopBar components to avoid code redundance

--- Code/Mobile_iOS/Immotep/Sources/Components/CustomTextInput.swift
+++ Code/Mobile_iOS/Immotep/Sources/Components/CustomTextInput.swift
@@ -10,24 +10,24 @@
 import SwiftUI
 
 struct CustomTextInput: View {
-    var title: LocalizedStringKey
-    var placeholder: LocalizedStringKey
+    var title: String
+    var placeholder: String
     @Binding var text: String
     var isSecure: Bool = false
 
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
-            Text(title)
+            Text(title.localized())
                 .font(.system(size: 14))
                 .frame(alignment: .leading)
 
             if isSecure {
-                SecureField(placeholder, text: $text)
+                SecureField(placeholder.localized(), text: $text)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .font(.system(size: 14))
                     .autocapitalization(.none)
             } else {
-                TextField(placeholder, text: $text)
+                TextField(placeholder.localized(), text: $text)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .font(.system(size: 14))
                     .autocapitalization(.none)
