7117111436b176a74eaf28ad27deb983ce44015a
Lucas Binder <lucas.binder@epitech.eu>
2024-11-25 15:15:27 -0500

    feature(invite tenant) : add route to invite tenant + create button and modal to add a tenant, waiting for routes implementation for the properties to use a existant ID for the tenant invitation

---
+++ Code/Web/src/components/DetailsPage/InviteTenantModal.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Modal, Form, Input, DatePicker, Button, FormProps, message } from 'antd'
+import { useTranslation } from 'react-i18next'
+
+import { InviteTenant, InviteTenantModalProps } from '@/interfaces/Tenant/InviteTenant.ts'
+import InviteTenants from '@/services/api/Tenant/InviteTenant.ts'
+
+const InviteTenantModal: React.FC<InviteTenantModalProps> = ({ isOpen, onClose }) => {
+    const { t } = useTranslation()
+
+    const onFinish: FormProps<InviteTenant>['onFinish'] = async (tenantInfo) => {
+        try {
+            const formattedTenantInfo = {
+                ...tenantInfo,
+
+                // waiting real ID's from the detailPage
+                propertyId: "cm3xcwfrs000512r6oh1dt0nm",
+            }
+            await InviteTenants(formattedTenantInfo)
+            message.success(t('pages.realPropertyDetails.inviteTenant'))
+            onClose()
+        } catch (error: any) {
+            if (error.response.status === 409)
+                message.error(t('pages.realPropertyDetails.409Error'))
+        }
+    }
+
+    const onFinishFailed: FormProps<InviteTenant>['onFinishFailed'] = (errorInfo) => {
+        message.error(t('pages.realPropertyDetails.fillFields'));
+    }
+
+    return (
+        <Modal
+            title={t('components.button.addTenant')}
+            open={isOpen}
+            onCancel={onClose}
+            footer={null}
+        >
+            <Form
+                name="invite_tenant"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                layout="vertical"
+                style={{ width: '90%', maxWidth: '500px', margin: '20px' }}
+            >
+                <Form.Item
+                    label={t('components.input.email.label')}
+                    name="tenant_email"
+                    rules={[
+                        { required: true, message: t('form.error.email') },
+                        { type: 'email', message: t('form.error.valid_email') },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label={t('components.input.startDate.label')}
+                    name="start_date"
+                    rules={[{ required: true, message: t('form.error.start_date') }]}
+                >
+                    <DatePicker style={{ width: '100%' }} />
+                </Form.Item>
+
+                <Form.Item
+                    label={t('components.input.endDate.label')}
+                    name="end_date"
+                    rules={[{ required: true, message: t('form.error.end_date') }]}
+                >
+                    <DatePicker style={{ width: '100%' }} />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        {t('components.button.add')}
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Modal>
+    )
+}
+
+export default InviteTenantModal
--- Code/Web/src/views/RealProperty/details/RealPropertyDetails.tsx
+++ Code/Web/src/views/RealProperty/details/RealPropertyDetails.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import { Button, Tooltip } from 'antd'
+import {
+  Button,
+} from 'antd'
 import { useTranslation } from 'react-i18next'
 
-import returnIcon from '@/assets/icons/return.png'
-
+import InviteTenantModal from '@/components/DetailsPage/InviteTenantModal'
 import style from './RealPropertyDetails.module.css'
 
 const RealPropertyDetails: React.FC = () => {
@@ -12,9 +13,19 @@ const RealPropertyDetails: React.FC = () => {
   const location = useLocation()
   const { id } = location.state || {}
 
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const showModal = () => setIsModalOpen(true)
+  const handleCancel = () => setIsModalOpen(false)
+
   return (
     <div className={style.pageContainer}>
       <span>Details for Real Property ID: {id}</span>
+      <Button type="primary" onClick={showModal}>
+        {t('components.button.addTenant')}
+      </Button>
+
+      <InviteTenantModal isOpen={isModalOpen} onClose={handleCancel} />
     </div>
   )
 }
