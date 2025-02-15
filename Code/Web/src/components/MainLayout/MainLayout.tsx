f0261024bf92778d0cf4108038af415a2d2bd23a
Oscar FRANK <oscar.frank@epitech.eu>
2025-02-15 16:20:36 +0100

    refactor(main layout) : menu refactored for new design && responsive handling

--- Code/Web/src/components/MainLayout/MainLayout.tsx
+++ Code/Web/src/components/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate, Outlet, useLocation } from 'react-router-dom'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 
 import Property from '@/assets/icons/realProperty.png'
 import Overview from '@/assets/icons/overview.png'
@@ -9,28 +9,30 @@ import Messages from '@/assets/icons/messages.png'
 import Immotep from '@/assets/icons/ImmotepLogo.svg'
 import Settings from '@/assets/icons/settings.png'
 import NavigationEnum from '@/enums/NavigationEnum'
+import type { MenuProps } from 'antd'
+import { t } from 'i18next'
 import style from './MainLayout.module.css'
 
 const { Content } = Layout
 
 const items = [
   {
-    label: 'components.button.overview',
+    label: t('components.button.overview'),
     key: NavigationEnum.OVERVIEW,
     icon: <img src={Overview} alt="Overview" className={style.menuIcon} />
   },
   {
-    label: 'components.button.real_property',
+    label: t('components.button.real_property'),
     key: NavigationEnum.REAL_PROPERTY,
     icon: <img src={Property} alt="Real Property" className={style.menuIcon} />
   },
   {
-    label: 'components.button.messages',
+    label: t('components.button.messages'),
     key: NavigationEnum.MESSAGES,
     icon: <img src={Messages} alt="Messages" className={style.menuIcon} />
   },
   {
-    label: 'components.button.settings',
+    label: t('components.button.settings'),
     key: NavigationEnum.SETTINGS,
     icon: <img src={Settings} alt="Settings" className={style.menuIcon} />
   }
@@ -41,7 +43,7 @@ const MainLayout: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false)
 
   const location = useLocation()
-  const currentLocation = location.pathname
+  const currentLocation = `/${location.pathname.split('/')[1] || ''}`
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen)
@@ -53,6 +55,10 @@ const MainLayout: React.FC = () => {
     ...item,
     label: t(item.label)
   }))
+
+  const onClick: MenuProps['onClick'] = e => {
+    navigate(e.key)
+  }
 
   return (
     <div className={style.pageContainer}>
@@ -79,30 +85,16 @@ const MainLayout: React.FC = () => {
             <div className={style.burgerLine} />
           </div>
 
-          {translatedItems.map(item => (
-            <span
-              className={`${style.buttonText} ${currentLocation === item.key ? style.active : ''}`}
-              onClick={() => navigate(item.key)}
-              onKeyDown={e => {
-                if (e.key === 'Enter' || e.key === ' ') {
-                  navigate(item.key)
-                }
-              }}
-              tabIndex={0}
-              aria-label="button"
-              role="button"
-              key={item.key}
-            >
-              {t(item.label)}
-            </span>
-          ))}
-
           {menuOpen && (
             <div className={style.menuDropdown}>
               {translatedItems.map(item => (
                 <div
                   key={item.key}
-                  className={style.menuItem}
+                  className={
+                    currentLocation === item.key
+                      ? style.menuItemActive
+                      : style.menuItem
+                  }
                   onClick={() => {
                     navigate(item.key)
                     setMenuOpen(false)
@@ -126,8 +118,24 @@ const MainLayout: React.FC = () => {
         </div>
       </div>
 
-      <Layout style={{ height: '100%', width: '100%', paddingTop: '70px' }}>
-        <Content style={{ height: '100%', width: '100%' }}>
+      <Layout
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row'
+        }}
+      >
+        <Menu
+          onClick={onClick}
+          style={{ width: 256, paddingTop: '70px' }}
+          defaultSelectedKeys={[NavigationEnum.OVERVIEW]}
+          selectedKeys={[currentLocation]}
+          mode="inline"
+          items={items}
+          className={style.menu}
+        />
+        <Content style={{ height: '100%', width: '100%', paddingTop: '70px' }}>
           <Outlet />
         </Content>
       </Layout>
