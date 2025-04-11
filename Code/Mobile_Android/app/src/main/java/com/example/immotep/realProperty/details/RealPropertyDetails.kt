a1eee7b862b53a0dadecf8954f54b9e4f00fe855
rdenni <robin.denni@epitech.eu>
2025-04-12 08:13:45 +0900

    feat(RealPropertyDetails&&TabsLayout) add the handling of the tabs in the real property details

--- Code/Mobile_Android/app/src/main/java/com/example/immotep/realProperty/details/RealPropertyDetails.kt
+++ Code/Mobile_Android/app/src/main/java/com/example/immotep/realProperty/details/RealPropertyDetails.kt
@@ -2,7 +2,6 @@ package com.example.immotep.realProperty.details
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,22 +10,16 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.AllOut
-import androidx.compose.material.icons.outlined.AttachFile
 import androidx.compose.material.icons.outlined.CalendarMonth
 import androidx.compose.material.icons.outlined.CalendarViewMonth
-import androidx.compose.material.icons.outlined.ChevronLeft
 import androidx.compose.material.icons.outlined.EditNote
-import androidx.compose.material.icons.outlined.MailOutline
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -40,24 +33,20 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.FileProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -65,161 +54,53 @@ import com.example.immotep.LocalApiService
 import com.example.immotep.R
 import com.example.immotep.addOrEditPropertyModal.AddOrEditPropertyModal
 import com.example.immotep.apiCallerServices.DetailedProperty
-import com.example.immotep.apiCallerServices.Document
-import com.example.immotep.apiCallerServices.PropertyStatus
 import com.example.immotep.components.ErrorAlert
 import com.example.immotep.components.InitialFadeIn
 import com.example.immotep.components.InternalLoading
-import com.example.immotep.components.LoadingDialog
 import com.example.immotep.inviteTenantModal.InviteTenantModal
-import com.example.immotep.realProperty.PropertyBox
+import com.example.immotep.layouts.TabsLayout
 import com.example.immotep.realProperty.PropertyBoxTextLine
 import com.example.immotep.realProperty.PropertyStatusBox
+import com.example.immotep.realProperty.details.tabs.AboutPropertyTab
+import com.example.immotep.realProperty.details.tabs.Damages
+import com.example.immotep.realProperty.details.tabs.DocumentBox
+import com.example.immotep.realProperty.details.tabs.OneDocument
 import com.example.immotep.ui.components.BackButton
-import com.example.immotep.utils.Base64Utils
 import com.example.immotep.utils.DateFormatter
-import com.example.immotep.utils.PdfsUtils
 
-@Composable
-fun OneDocument(document: Document, openPdf: (String) -> Unit) {
-    InitialFadeIn(durationMs = 500) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth(0.33f)
-                .padding(5.dp)
-                .clickable {
-                    openPdf(document.id)
-                }
-                .wrapContentSize(Alignment.Center)
-                .testTag("OneDocument ${document.id}")
-        ) {
-            Box(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.background)
-                    .border(
-                        1.dp,
-                        color = MaterialTheme.colorScheme.background,
-                        shape = RoundedCornerShape(5.dp)
-                    )
-                    .padding(start = 25.dp, end = 25.dp, top = 10.dp, bottom = 10.dp)
-            ) {
-                Icon(
-                    Icons.Outlined.AttachFile,
-                    contentDescription = "document icon",
-                    modifier = Modifier.size(50.dp)
-                )
-            }
-            Text(
-                text = document.name,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(start = 10.dp, end = 10.dp).fillMaxWidth(),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
-    }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun DocumentBox(property : State<DetailedProperty>, openPdf: (String) -> Unit) {
-    Text(text = stringResource(R.string.documents))
-    Box(
-        modifier = Modifier.fillMaxWidth()
-            .border(
-                1.dp,
-                color = MaterialTheme.colorScheme.onBackground,
-                shape = RoundedCornerShape(5.dp)
-            )
-            .background(MaterialTheme.colorScheme.tertiaryContainer)
-            .padding(5.dp)
-
-    ) {
-        FlowRow(modifier = Modifier.defaultMinSize(minHeight = 125.dp)) {
-            property.value.documents.forEach { item ->
-                OneDocument(item, openPdf = { openPdf(it)})
-            }
-        }
-    }
-}
-
-@Composable
-fun AboutThePropertyBox(property : State<DetailedProperty>, openEdit : () -> Unit) {
-    Text(text = stringResource(R.string.about_the_property))
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(1.dp, color = MaterialTheme.colorScheme.onBackground, shape = RoundedCornerShape(5.dp))
-            .padding(5.dp)
-    ) {
-        Column(modifier = Modifier.fillMaxWidth(0.5f)) {
-            PropertyBoxTextLine(property.value.tenant?: "", Icons.Outlined.AccountBox)
-            PropertyBoxTextLine(
-                DateFormatter.formatOffsetDateTime(property.value.startDate) ?:
-                "---------------------",
-                Icons.Outlined.CalendarMonth
-            )
-            PropertyBoxTextLine(
-                (
-                        DateFormatter.formatOffsetDateTime(property.value.endDate) ?:
-                        "---------------------"
-                        ),
-                Icons.Outlined.CalendarMonth
-            )
-        }
-        Column(modifier = Modifier.fillMaxWidth()) {
-            PropertyBoxTextLine("${stringResource(R.string.area)}: ${property.value.area} m²", Icons.Outlined.AllOut)
-            PropertyBoxTextLine(
-                "${stringResource(R.string.rentMonth)}: ${property.value.rent}€",
-                Icons.Outlined.CalendarViewMonth,
-            )
-            PropertyBoxTextLine(
-                "${stringResource(R.string.deposit)}: ${property.value.deposit}€",
-                Icons.Outlined.EditNote,
-            )
-        }
-    }
-    Spacer(modifier = Modifier.height(10.dp))
-    Button(
-        onClick = { openEdit() },
-        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
-        modifier = Modifier
-            .clip(RoundedCornerShape(5.dp))
-            .padding(5.dp)
-            .fillMaxWidth()
-            .testTag("editProperty")
-    ) {
-        Text(
-            stringResource(R.string.edit_property),
-            color = MaterialTheme.colorScheme.onTertiary
-        )
-    }
-    Spacer(modifier = Modifier.height(10.dp))
-}
 
 @Composable
 fun RealPropertyDetailsScreen(navController: NavController, newProperty : DetailedProperty, getBack: (DetailedProperty) -> Unit) {
     val apiService = LocalApiService.current
     val context = LocalContext.current
+    val tabs = listOf(
+        stringResource(R.string.about),
+        stringResource(R.string.documents),
+        stringResource(R.string.damages)
+    )
+
 
     val viewModel: RealPropertyDetailsViewModel = viewModel {
         RealPropertyDetailsViewModel(navController, apiService)
     }
     val property = viewModel.property.collectAsState()
     var editOpen by rememberSaveable { mutableStateOf(false) }
+    var tabIndex by rememberSaveable { mutableIntStateOf(0) }
     var inviteTenantOpen by rememberSaveable { mutableStateOf(false) }
 
     val apiErrors = viewModel.apiError.collectAsState()
     val isLoading = viewModel.isLoading.collectAsState()
+
     val errorAlertVal = when (apiErrors.value) {
         RealPropertyDetailsViewModel.ApiErrors.GET_PROPERTY -> stringResource(R.string.api_error_get_property)
         RealPropertyDetailsViewModel.ApiErrors.UPDATE_PROPERTY -> stringResource(R.string.api_error_edit_property)
         else -> null
     }
+
     LaunchedEffect(newProperty) {
         viewModel.loadProperty(newProperty)
     }
+
     AddOrEditPropertyModal(
         open = editOpen,
         close = { editOpen = false },
@@ -241,7 +122,7 @@ fun RealPropertyDetailsScreen(navController: NavController, newProperty : Detail
         return
     }
     InitialFadeIn(300) {
-        Column(modifier = Modifier.padding(5.dp).testTag("realPropertyDetailsScreen")) {
+        Column(modifier = Modifier.testTag("realPropertyDetailsScreen")) {
             Box(modifier = Modifier.fillMaxWidth()) {
                 AsyncImage(
                     model = property.value.image,
@@ -278,7 +159,7 @@ fun RealPropertyDetailsScreen(navController: NavController, newProperty : Detail
             }
             Spacer(modifier = Modifier.height(10.dp))
             ErrorAlert(null, null, errorAlertVal)
-            Column(modifier = Modifier.padding(20.dp)) {
+            Column(modifier = Modifier.background(MaterialTheme.colorScheme.background).padding(20.dp)) {
                 Box(modifier = Modifier.fillMaxWidth()) {
                     PropertyStatusBox(property.value.status, modifier = Modifier.padding(end = 5.dp).align(Alignment.TopEnd))
                 }
@@ -287,6 +168,13 @@ fun RealPropertyDetailsScreen(navController: NavController, newProperty : Detail
                     fontSize = 15.sp,
                     fontWeight = FontWeight.Thin
                 )
+            }
+            TabsLayout(tabIndex, tabs, { tabIndex = it }) {
+                when (tabIndex) {
+                    0 -> AboutPropertyTab(property, openEdit = { editOpen = true })
+                    1 -> DocumentBox(property = property, openPdf = { viewModel.openPdf(it, context)})
+                    2 -> Damages()
+                }
             }
 
 
