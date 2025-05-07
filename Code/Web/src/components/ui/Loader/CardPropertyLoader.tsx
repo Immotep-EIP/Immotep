aebdca29750128d3c7fe6aa85af62e428f1fb1b8
Lucas Binder <lucas.binder@epitech.eu>
2025-05-07 22:26:36 +0200

    refactor (component loader) updated to reflect the new ui

--- Code/Web/src/components/ui/Loader/CardPropertyLoader.tsx
+++ Code/Web/src/components/ui/Loader/CardPropertyLoader.tsx
@@ -1,34 +1,36 @@
 import React from 'react'
-import { Skeleton } from 'antd'
+import { Badge, Skeleton } from 'antd'
 
-import style from '@/views/RealProperty/RealProperty.module.css'
+import style from '@/components/features/RealProperty/PropertyCard.module.css'
 
-const CardPropertyLoader: React.FC<{ cards: number }> = ({ cards }) => (
-  <div className={style.cardsContainer}>
-    {Array(cards)
-      .fill(0)
-      .map((_, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div className={style.card} key={index}>
-          <div className={style.cardContentContainer}>
+const CardPropertyLoader: React.FC<{ cards: number }> = ({ cards }) =>
+  Array(cards)
+    .fill(0)
+    .map((_, index) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <div className={style.card} key={index}>
+        <div className={style.cardContentContainer}>
+          <Badge.Ribbon
+            text={<Skeleton.Input size="small" active />}
+            color="lightgray"
+          >
             <div className={style.cardPictureContainer}>
               <Skeleton.Image active className={style.cardPicture} />
             </div>
-            <div className={style.cardInfoContainer}>
-              <b className={style.cardText}>
+          </Badge.Ribbon>
+          <div className={style.cardInfoContainer}>
+            <b className={style.cardText}>
+              <Skeleton.Input size="small" active />
+            </b>
+            <div className={style.cardAddressContainer}>
+              <Skeleton.Avatar size={20} active />
+              <span className={style.cardText}>
                 <Skeleton.Input size="small" active />
-              </b>
-              <div className={style.cardAddressContainer}>
-                <Skeleton.Avatar size={20} active />
-                <span className={style.cardText}>
-                  <Skeleton.Input size="small" active />
-                </span>
-              </div>
+              </span>
             </div>
           </div>
         </div>
-      ))}
-  </div>
-)
+      </div>
+    ))
 
 export default CardPropertyLoader
