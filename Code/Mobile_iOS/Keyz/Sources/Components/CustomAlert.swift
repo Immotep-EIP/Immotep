e5e47393a78e060284a74425d89a483e7f3f32c6
AlessioLieb <ALESSIO2003@OUTLOOK.FR>
2025-05-20 23:16:30 +0200

    feat(new custom alert): added new custom alert component, with one custom textfield + one dropdown (used only in add room, see if useful anywhere else)

--- Code/Mobile_iOS/Keyz/Sources/Components/CustomAlert.swift
+++ Code/Mobile_iOS/Keyz/Sources/Components/CustomAlert.swift
@@ -340,7 +340,6 @@ struct CustomAlertWithTwoTextFields: View {
         }
         .ignoresSafeArea()
     }
-
     func close() {
         withAnimation(.spring()) {
             offset = 1000
@@ -348,6 +347,172 @@ struct CustomAlertWithTwoTextFields: View {
         }
     }
 }
+
+    struct CustomAlertWithTextAndDropdown: View {
+        @Binding var isActive: Bool
+        @Binding var textFieldInput: String
+        @State private var selectedRoomType: String
+        let roomTypes = [
+            "dressing",
+            "laundryroom",
+            "bedroom",
+            "playroom",
+            "bathroom",
+            "toilet",
+            "livingroom",
+            "diningroom",
+            "kitchen",
+            "hallway",
+            "balcony",
+            "cellar",
+            "garage",
+            "storage",
+            "office",
+            "other"
+        ]
+
+        let title: String
+        let message: String
+        let buttonTitle: String
+        let secondaryButtonTitle: String?
+        let action: (String, String) -> Void
+        let secondaryAction: (() -> Void)?
+
+        @State private var offset: CGFloat = 1000
+
+        init(
+            isActive: Binding<Bool>,
+            textFieldInput: Binding<String>,
+            title: String,
+            message: String,
+            buttonTitle: String,
+            secondaryButtonTitle: String?,
+            action: @escaping (String, String) -> Void,
+            secondaryAction: (() -> Void)?
+        ) {
+            self._isActive = isActive
+            self._textFieldInput = textFieldInput
+            self.title = title
+            self.message = message
+            self.buttonTitle = buttonTitle
+            self.secondaryButtonTitle = secondaryButtonTitle
+            self.action = action
+            self.secondaryAction = secondaryAction
+            self._selectedRoomType = State(initialValue: roomTypes.first ?? "other")
+        }
+
+        var body: some View {
+            ZStack {
+                Color(.black)
+                    .opacity(0.5)
+                    .onTapGesture {
+                        close()
+                    }
+
+                VStack {
+                    Text(title)
+                        .font(.title2)
+                        .bold()
+                        .padding()
+                        .foregroundStyle(Color("textColor"))
+
+                    Text(message)
+                        .font(.body)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 10)
+                        .foregroundStyle(Color("textColor"))
+
+                    TextField("Enter room name", text: $textFieldInput)
+                        .frame(height: 35)
+                        .background(RoundedRectangle(cornerRadius: 8).fill(Color("textfieldBackground")))
+                        .padding(.horizontal)
+
+                    Picker("Room Type", selection: $selectedRoomType) {
+                        ForEach(roomTypes, id: \.self) { type in
+                            Text(type.capitalized)
+                                .tag(type)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .padding(.horizontal)
+                    .padding(.vertical, 5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                    )
+
+                    HStack {
+                        if let secondaryButtonTitle = secondaryButtonTitle {
+                            Button {
+                                secondaryAction?()
+                                close()
+                            } label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .foregroundColor(.gray)
+
+                                    Text(secondaryButtonTitle)
+                                        .font(.system(size: 16, weight: .bold))
+                                        .foregroundColor(.white)
+                                        .padding()
+                                }
+                                .frame(height: 50)
+                            }
+                        }
+
+                        Button {
+                            action(textFieldInput, selectedRoomType)
+                            close()
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .foregroundColor(.red)
+
+                                Text(buttonTitle)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(.white)
+                                    .padding()
+                            }
+                            .frame(height: 50)
+                        }
+                        .disabled(textFieldInput.isEmpty)
+                    }
+                    .padding()
+                }
+                .fixedSize(horizontal: false, vertical: true)
+                .padding()
+                .background(Color("basicWhiteBlack"))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .overlay(alignment: .topTrailing) {
+                    Button {
+                        close()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.title2)
+                            .fontWeight(.medium)
+                    }
+                    .tint(Color("textColor"))
+                    .padding()
+                }
+                .shadow(radius: 20)
+                .padding(30)
+                .offset(x: 0, y: offset)
+                .onAppear {
+                    withAnimation(.spring()) {
+                        offset = 0
+                    }
+                }
+            }
+            .ignoresSafeArea()
+        }
+
+        func close() {
+            withAnimation(.spring()) {
+                offset = 1000
+                isActive = false
+            }
+        }
+    }
 
 struct CustomAlert_Previews: PreviewProvider {
     static var previews: some View {
@@ -375,6 +540,21 @@ struct CustomAlert_Previews: PreviewProvider {
                 secondaryButtonTitle: "Annuler",
                 action: {
                     print("Confirmation action")
+                },
+                secondaryAction: {
+                    print("Annulation action")
+                }
+            )
+
+            CustomAlertWithTextAndDropdown(
+                isActive: .constant(true),
+                textFieldInput: .constant(""),
+                title: "Alerte avec Texte et Dropdown",
+                message: "Entrez un nom et sélectionnez un type :",
+                buttonTitle: "Ajouter",
+                secondaryButtonTitle: "Annuler",
+                action: { name, type in
+                    print("Nom: \(name), Type: \(type)")
                 },
                 secondaryAction: {
                     print("Annulation action")
