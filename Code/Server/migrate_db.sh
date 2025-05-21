f0629c3a688ccf84e95eec8d6f9fd38d337abb2b
Mazettt <martin.d-herouville@epitech.eu>
2025-05-21 15:54:49 +0200

    build(backend): remove each shell script and add Makefile with all scripts inside

--- Code/Server/migrate_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# -e Exit immediately when a command returns a non-zero status.
-# -x Print commands before they are executed
-set -ex
-
-go run github.com/steebchen/prisma-client-go migrate dev
