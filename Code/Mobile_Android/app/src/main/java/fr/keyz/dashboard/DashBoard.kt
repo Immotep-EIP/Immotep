706d5869ed4200d415d926bfbb5aeb0d353ff7d2
rdenni <robin.denni@epitech.eu>
2025-07-05 15:34:01 +0900

    feat(DashBoard) add the base for the tenant dashboard and a way to get to it

--- Code/Mobile_Android/app/src/main/java/fr/keyz/dashboard/DashBoard.kt
+++ Code/Mobile_Android/app/src/main/java/fr/keyz/dashboard/DashBoard.kt
@@ -1,55 +1,19 @@
 package fr.keyz.dashboard
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import fr.keyz.LocalApiService
-import fr.keyz.components.InitialFadeIn
-import fr.keyz.components.InternalLoading
-import fr.keyz.dashboard.widgets.DamageWidget
-import fr.keyz.dashboard.widgets.DamagesListWidget
-import fr.keyz.dashboard.widgets.HelloWidget
-import fr.keyz.dashboard.widgets.PropertiesWidget
-import fr.keyz.dashboard.widgets.RemindersWidget
+import fr.keyz.LocalIsOwner
+import fr.keyz.dashboard.tenant.TenantDashBoard
+
 
 @Composable
 fun DashBoardScreen(
     navController: NavController,
 ) {
-    val apiService = LocalApiService.current
-    val viewModel : DashBoardViewModel = viewModel {
-        DashBoardViewModel(navController, apiService)
-    }
-    val isLoading = viewModel.isLoading.collectAsState()
-    val dashBoard = viewModel.dashBoard.collectAsState()
-    val userName = viewModel.userName.collectAsState()
-
-    LaunchedEffect(Unit) {
-        viewModel.getDashBoard()
-        viewModel.getName()
-    }
-
-    DashBoardLayout(navController, "dashboardScreen") {
-        if (isLoading.value) {
-            InternalLoading()
-            return@DashBoardLayout
-        }
-        InitialFadeIn(durationMs = 200) {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                HelloWidget(dashBoard.value.properties.nbrTotal, userName = userName.value)
-                RemindersWidget(dashBoard.value.reminders)
-                PropertiesWidget(dashBoard.value.properties)
-                //UnreadMessagesWidget()
-                //ScheduledInventoryWidget()
-                DamageWidget(dashBoard.value.openDamages)
-                DamagesListWidget(dashBoard.value.openDamages.listToFix)
-            }
-        }
+    val isOwner = LocalIsOwner.current.value
+    if (isOwner) {
+        DashBoardScreenOwner(navController)
+    } else {
+        TenantDashBoard(navController)
     }
 }
---
+++ Code/Mobile_Android/app/src/main/java/fr/keyz/dashboard/tenant/TenantDashBoard.kt
@@ -0,0 +1,9 @@
+package fr.keyz.dashboard.tenant
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun TenantDashBoard(navController: NavController) {
+
+}
